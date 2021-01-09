f9dfb1c1aa0ac84b140ad27db395f1908fd91326
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-01-08 16:17:54 -0800

    adding alert if nothing is typed in textarea

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NavBar from './components/layout/NavBar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
+import alert, { Alert } from './components/layout/Alert'
 import './App.css';
 import axios from 'axios'
 
@@ -9,7 +10,8 @@ class App extends Component {
   state = {
     //initial states for users and loading
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
 
@@ -29,12 +31,21 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   }
 
+  // set alert if nothing is typed
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg:msg, type:type } });
+
+    // Timeout to remove alert message after five seconds
+    setTimeout(() => this.setState({ alert: null}), 5000);
+  }
+
   render() {
     return (
       <div className="App">
         <NavBar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true: false}/>
+          <Alert alert={this.state.alert} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true: false} setAlert={this.setAlert}/>
         <Users loading={this.state.loading} users={this.state.users} />
         </div>
 
