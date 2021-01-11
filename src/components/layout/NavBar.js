72ed37002f169b28c2e48e7e70093e96810da328
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-01-10 17:39:17 -0800

    adding home and about hrefs in Navbar using Link

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
+//The reason why we use Link is because when we do search and go to the about page and then return back link will save the search results, where as in a tag the search results will be gone.
 
 const NavBar = (props) => {
   //navbar where the title will be shown with the background color and github icon
@@ -10,6 +11,15 @@ const NavBar = (props) => {
         <h1>
           <i className={props.icon} />  {props.title}
         </h1>
+        <ul>
+          <li>
+           <Link to='/'>Home</Link> 
+            
+          </li>
+          <li>
+          <Link to='/about'>About</Link> 
+          </li>
+        </ul>
       </nav>
     )
 }
