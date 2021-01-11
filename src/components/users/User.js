3c9b2e9447abdd546ab108781cacce7e0842dcc2
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-01-10 22:14:35 -0800

    showing the data on the user's page including name, repos and link to the github profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -18,7 +18,7 @@ export class User extends Component {
 
   render() {
     //taking all the stuffs from api that are passed as props.
-    const {name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = this.props.user
+    const {name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable, company } = this.props.user
 
     const {loading} = this.props
 
@@ -43,8 +43,36 @@ export class User extends Component {
             <p>{bio}</p>
           </Fragment>
           )}
+          <a href={html_url} className='btn btn-dark my-1' >
+            Visit Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && <Fragment>
+                <strong>Username:</strong> {login}  
+              </Fragment>}
+            </li>
+            <li>
+              {company && <Fragment>
+                <strong>Company:</strong> {company}  
+              </Fragment>}
+            </li>
+            <li>
+              {blog && <Fragment>
+                <strong>Website:</strong> {blog}  
+              </Fragment>}
+            </li>
+          </ul>
         </div>
       </div>
+
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      </div>
+
     </Fragment>
         
   }
